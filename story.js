f7508c6868d35b308d7dcc76dabfeb6c01b056b4
brickrend <Brickrends9@gmail.com>
2021-05-24 18:29:01 +0300

    Done!

---
+++ story.js
@@ -0,0 +1,5 @@
+const name = "jack"
+const age = 25
+const hobby = "Gaming"
+
+console.log (`Mr./Mrs. ${name}, in 33 years, you will be ${age + 33}. By then you'll have decades of ${hobby} practice!`)
